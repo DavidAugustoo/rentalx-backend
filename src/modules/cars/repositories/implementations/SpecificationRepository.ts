7e0c344b51fc9b2f63e3204b96bbe7774782f2cd
David Augusto <davidribeiro086@gmail.com>
2022-12-06 22:07:30 -0300

    refactor: creating useCase for creating specs

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,14 +1,23 @@
-import { Specification } from "../model/Specification";
+import { Specification } from "../../model/Specification";
 import {
     ICreateSpecificationDTO,
     ISpecificationRepository,
-} from "./ISpecificationRepository";
+} from "../ISpecificationRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
     private specifications: Specification[];
 
+    private static INSTANCE: SpecificationRepository;
+
     constructor() {
         this.specifications = [];
+    }
+    static getInstance(): SpecificationRepository {
+        if (!SpecificationRepository.INSTANCE) {
+            SpecificationRepository.INSTANCE = new SpecificationRepository();
+        }
+
+        return SpecificationRepository.INSTANCE;
     }
 
     create({ name, description }: ICreateSpecificationDTO): void {
