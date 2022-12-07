9f21f6e9083930bed5414c7a97feed3aaf295fee
David Augusto <davidribeiro086@gmail.com>
2022-12-07 09:33:17 -0300

    refactor: modularizing the routes file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { router } from "./routes";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specification.routes";
 
@@ -7,7 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
