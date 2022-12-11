d82b9cbc989c07388df000a429d73834abc83a67
David Augusto <davidribeiro086@gmail.com>
2022-12-10 22:43:55 -0300

    refactor: refactoring a category template

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
 
-const dataSource = new DataSource({
+export const dataSource = new DataSource({
     type: "postgres",
-    host: "database", // Nome dado ao service do banco de dados
+    host: "localhost", // Nome dado ao service do banco de dados
     port: 5432,
     username: "docker",
     password: "ignite",
     database: "rentx",
+    migrations: ["src/database/migrations/*.ts"],
 });
 
 dataSource
