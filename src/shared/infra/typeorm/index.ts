4127310cec477ce0ace211a89a896bf569e45454
David Augusto <davidribeiro086@gmail.com>
2022-12-26 21:39:24 -0300

    build: script added to build database tables

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,24 @@
 import { DataSource } from "typeorm";
 
+enum Host {
+    test = "localhost",
+    typeorm = "localhost",
+    typeorm_test = "localhost",
+}
+
+enum Database {
+    test = "rentx_test",
+    typeorm = "rentx",
+    typeorm_test = "rentx_test",
+}
+
 export const dataSource = new DataSource({
     type: "postgres",
-    host: process.env.NODE_ENV === "test" ? "localhost" : "database", // Nome dado ao service do banco de dados
+    host: Host[process.env.NODE_ENV] ?? "database", // Nome dado ao service do banco de dados
     port: 5432,
     username: "docker",
     password: "ignite",
-    database: process.env.NODE_ENV === "test" ? "rentx_test" : "rentx",
+    database: Database[process.env.NODE_ENV] ?? "rentx",
     entities: ["./src/modules/**/entities/*.ts"],
     migrations: ["src/shared/infra/typeorm/migrations/*.ts"],
 });
